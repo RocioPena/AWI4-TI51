c663c06145fb522ac10f2af1b68edb8725ee42dd
RocioPena <78064085+RocioPena@users.noreply.github.com>
2023-01-13 12:35:48 -0600

    Base_web.py

---
+++ awi4/app.py
@@ -0,0 +1,21 @@
+import web
+
+urls = (
+    '/', 'Index', 
+    '/bienvenida', 'Bienvenida'
+)
+app = web.application(urls, globals())
+render = web.template.render('templates')
+
+class Index:
+    def GET(self):
+       
+        return render.index()
+
+class Bienvenida:
+    def GET(self):
+
+        return render.bienvenida()
+
+if __name__ == "__main__":
+    app.run()
