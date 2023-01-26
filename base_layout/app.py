8fdc388e623266d845526432ea56cf8a07bf01d3
ROCIO PE�A <78064085+RocioPena@users.noreply.github.com>
2023-01-26 14:53:36 +0000

    base de un layout

---
+++ base_layout/app.py
@@ -0,0 +1,20 @@
+import web
+
+urls = (
+    '/',
+    'Tablas',
+)
+app = web.application(urls, globals())
+render = web.template.render('templates', base='layout')
+
+
+class Tablas:
+
+    def GET(self):
+        datos = [["1", "Dejah"], ["2", "Jhon"]]
+        return render.tabla(datos)
+
+
+if __name__ == "__main__":
+    web.config.debug = True
+    app.run()
